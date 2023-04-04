c48d4d734484743f676c0b7de942847085ebee4e
KNR1997 <kethakaranasinghe@gmail.com>
2023-04-05 01:45:29 +0530

    LA : initial files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import './App.css';
+import Footer from './layouts/NavbarAndFooter/Footer';
+import Navbar from './layouts/NavbarAndFooter/Navbar';
+import HomePage from './layouts/HomePage/HomePage';
+import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
+import { Redirect, Route, Switch } from 'react-router';
+import { Provider } from 'react-redux';
+import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Heloo!</h1>
+    <div className='d-flex flex-column min-vh-100'>
+      <Navbar />
+      <div className='flex-grow-1'>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/home' />
+          </Route>
+          <Route path='/home'>
+            <HomePage />
+          </Route>
+          <Route path='/search'>
+            <SearchBooksPage />
+          </Route>
+          <Route path='/checkout/:bookId'>
+            <BookCheckoutPage />
+          </Route>
+        </Switch>
+      </div>
+      <Footer />
     </div>
+
   );
 }
 
