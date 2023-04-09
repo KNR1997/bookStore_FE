31ff47c7d5defb45036e6a698cb5aab9f948a196
KNR1997 <kethakaranasinghe@gmail.com>
2023-04-09 17:05:23 +0530

    BS : okta config

---
+++ src/lib/oktaConfig.tsx
@@ -0,0 +1,8 @@
+export const oktaConfig = {
+    clientId: '0oa91q1b03bsRbwA95d7',
+    issuer: 'https://dev-79977778.okta.com/oauth2/default',
+    redirectUrl: 'http://localhost:3000/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+    pkce: true,
+    disableHttpsCheck: true,
+}
