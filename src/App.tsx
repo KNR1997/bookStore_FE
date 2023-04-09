3a0a592cb04647fdb2993e1f46605115af5a1d61
KNR1997 <kethakaranasinghe@gmail.com>
2023-04-09 17:35:09 +0530

    BS : Adding Configuration to App

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,26 @@ import Footer from './layouts/NavbarAndFooter/Footer';
 import Navbar from './layouts/NavbarAndFooter/Navbar';
 import HomePage from './layouts/HomePage/HomePage';
 import { SearchBooksPage } from './layouts/SearchBooksPage/SearchBooksPage';
-import { Redirect, Route, Switch } from 'react-router';
+import { Redirect, Route, Switch, useHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import { BookCheckoutPage } from './layouts/BookCheckoutPage/BookCheckoutPage';
+import { oktaConfig } from './lib/oktaConfig';
+import {OktaAuth, toRelativeUrl} from '@okta/okta-auth-js';
+
+const oktaAuth = new OktaAuth(oktaConfig); 
 
 function App() {
+
+const customAuthHandler = () => {
+  history.push('/login');
+}
+
+const history = useHistory();
+
+const restoreOriginalUri= async(_oktaAuth: any, originalUri: any) => {
+  history.replace(toRelativeUrl(originalUri || '/', window.location.origin));
+}
+
   return (
     <div className='d-flex flex-column min-vh-100'>
       <Navbar />
