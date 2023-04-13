9d872e5ea6e3707ca0db09fa36b77eaa7e8526aa
KNR1997 <kethakaranasinghe@gmail.com>
2023-04-13 09:37:33 +0530

    BS : oktaConfig issue fix

--- src/lib/oktaConfig.tsx
+++ src/lib/oktaConfig.tsx
@@ -1,7 +1,7 @@
 export const oktaConfig = {
     clientId: '0oa91q1b03bsRbwA95d7',
     issuer: 'https://dev-79977778.okta.com/oauth2/default',
-    redirectUrl: 'http://localhost:3000/login/callback',
+    redirectUri: 'http://localhost:3000/login/callback',
     scopes: ['openid', 'profile', 'email'],
     pkce: true,
     disableHttpsCheck: true,
